0c4ee37311faab933b6b9a3e528287d907abffbd
Voronskoy <marskoy27@gmail.com>
2025-06-06 17:37:39 +0300

    add indexJS + const,let

---
+++ index.js
@@ -0,0 +1,58 @@
+ console.log('hi, console!');
+
+///
+let wakeUptime = '10:45';
+console.log(wakeUptime);
+
+const amountDaysInWeek = 7;
+const days_in_week = 7;
+const allDayWeek = 7;
+console.log(allDayWeek);
+
+
+//
+
+const isWork = false;
+
+//
+const isCar = false;
+
+//
+const myHeight = 1.78;
+console.log(myHeight);
+
+
+
+
+let titleMovie = 'Gohstrider';
+console.log(titleMovie);
+titleMovie = 'Gohstrider2'
+
+ // create zminnu for your pets
+ let myPet = 'cat-Sima';
+ console.log(myPet);
+
+
+ let firstName = 'Mark';
+ console.log(firstName);
+ let isAdult = true;
+ let isWeekend = false;
+
+ //створіть змінну яка буде містити булевве значення про те чи є ви студентом
+ let isStudent = false;
+
+ let yearOFbirth = 2000;
+ yearOFbirth = 2009;
+ console.log(yearOFbirth);
+
+ //create zminnu for safe yoru favorite color
+ 
+let colorFavorite = 'blue';
+console.log(firstName, colorFavorite);
+
+let age = null;
+console.log(age);
+age = 15; 
+console.log(age);
+
+
