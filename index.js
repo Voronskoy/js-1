2ad020a6b3cd0e40db2041554ea387612d9d137a
Voronskoy <marskoy27@gmail.com>
2025-06-16 16:30:50 +0300

    ifelse_tasks

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
-// const accessAge = 12;
-// const userAge = prompt('Скільки вам років?');
-
-// if (Number(userAge) >= accessAge) {
-//   alert('Ви можете купляти квиток');
+// const yearCheck = 2000;
+// const birthYear = prompt("Ваш рік народження?");
+// // if (Number(birthYear) < yearCheck) {
+// //   alert("Ви народилися в минулому тисячолітті");
+// // } else {
+// //   alert("Ви з 3-ого тисячолітті");
+// // }
+// if (Number(birthYear) >= yearCheck) {
+//   alert("Ви з 3-ого тисячолітті");
 // } else {
-//   const yearWait = accessAge - Number(userAge);
-//   alert('Треба ще почекати років: ' + yearWait);
+//   alert("Ви народилися в минулому тисячолітті");
 // }
 
-const num1 = prompt('Enter first number'); // string
-const num2 = prompt('Enter second number'); // string
+// const userInput = prompt("Введіть Так/Ні");
+// // if (userInput === "Так") {
+// //   alert(";)");
+// // } else {
+// //   if (userInput === "Ні") {
+// //     alert(":(");
+// //   } else {
+// //     alert("???");
+// //   }
+// // }
+// if (userInput === "Так") {
+//   alert(";)");
+// } else if (userInput === "Ні") {
+//   alert(":(");
+// } else {
+//   alert("???");
+// }
 
-if (num1 > num2) { // 3 > 3 false
-  alert(num1 + ' - bigest');
-} else if (num2 > num1) { // 3 > 3 false
-  alert(num2 + ' - bigest');
+// const speedUser = prompt('Ваша швидкість?');
+// if (Number(speedUser) > 120) {
+//   alert('Штраф!');
+// } else if(Number(speedUser) >= 60){
+//   alert('Normal');
+// }
+// else{
+// alert('можна швидше!');
+// }
+
+const chargeLevel = prompt("Ваш заряд на телефоні?");
+const chargeNumber = Number(chargeLevel);
+// if (Number(chargeLevel) > 80) {
+//   alert("full");
+// } else if (Number(chargeLevel) >= 30) {
+//   alert("normal");
+// } else {
+//   alert("urgent charges");
+// }
+if (chargeNumber > 100) {
+  alert("it i imposible");
+} else if (chargeNumber > 80) {
+  alert("full");
+} else if (chargeNumber >= 30) {
+  alert("normal");
+} else if (chargeNumber > 0) {
+  alert("urgent charges");
 } else {
-  alert('Equal');
+  alert("rip");
 }
