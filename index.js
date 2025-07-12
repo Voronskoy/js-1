04c21da78e2ee7abb7308238c034deb3c9ee37a2
Voronskoy <marskoy27@gmail.com>
2025-07-12 14:56:19 +0300

    while2

--- index.js
+++ index.js
@@ -23,11 +23,39 @@
 // запитувати в користувача ввести число кратне 7 до тих пір, доки він не введе правильно
 // число % 7 === 0 -> break
 
+// while (true) {
+//   const userInput = prompt('enter number multiply 7');
+//   const userNumber = Number(userInput);
+//   if (userNumber % 7 === 0 && userInput !== '' && userInput !== null) {
+//     alert('thanks');
+//     break;
+//   }
+// }
+
+
+// while (true) {
+//   const userInput = prompt('Enter something');
+
+//   if (userInput !== '' && userInput !== null) {
+//     alert('thanks');
+//     break;
+//   }
+// }
+
+const secretNumber = 4;
+
 while (true) {
-    const userInput = prompt('enter number multiply 7');
-    const userNumber = Number(userInput);
-    if (userNumber % 7 === 0) {
-        alert('thanks');
-        break;
-    }
+  const userInput = prompt('Enter number');
+  const userNumber = Number(userInput);
+
+  if (userInput === null || userInput.trim() === '' || Number.isNaN(userNumber)) {
+    alert('Error! Enter a valid number');
+  } else if (userNumber === secretNumber) {
+    alert('You win!');
+    break;
+  } else if (userNumber > secretNumber) {
+    alert('Secret number is less');
+  } else {
+    alert('Secret number is bigger');
+  }
 }
