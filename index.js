4605617983aae04dcdbeeb43133a89bed3aa418f
Voronskoy <marskoy27@gmail.com>
2025-07-22 10:25:02 +0300

    add row after continue

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
-// користувач вводить початкове значеня - число
-// потім він вводить знаки або + або -
-// ми залежно від цих знаків робимо інкремент або дікремент
-// це все продовжуються до того доки користувач не натисне відміну
+const start = 15;
+const end = 20;
+const secretNumber = 17;
 
-// const userInput = prompt("Enter number:");
-// let userNumber = Number(userInput);
-// while (true) {
-//   const userInput2 = prompt("Enter +/-:");
+for (let i = start; i < end; i++) {
+    if (i === secretNumber) {
+        continue;
+        alert('after continue');
+    }
+    console.log(i);
+}
 
-//   if (userInput2 === null) {
-//     break;
-//   } else if (userInput2 === "+") {
-//     userNumber++;
-//   } else if (userInput2 === "-") {
-//     userNumber--;
-//   } else {
-//     alert("error");
-//   }
-// }
-// alert(userNumber);
+debugger;
+while (true) {
+    const userInput = prompt('Enter number');
+    
+    if (userInput === null) {
+        alert('будь уважний!');
+        continue;
+    }
 
-// for (let i = 3; i <= 9; i++) {
-//   console.log(i, "->", i * i);
-// }
-
-// for (let i = 1; i <= 10; i++) {
-//   if (i % 4 === 0) {
-//     console.log("***");
-//   } else if (i % 6 === 0) {
-//     console.log("$$$");
-//   } else {
-//     console.log(i);
-//   }
-// }
-let count = 0;
-while (count < 3) {
-  const userInput = prompt("enter number:");
-  if (Number(userInput) % 7 === 0) {
-    count++;
-    console.log(count, userInput);
-  }
+    if (Number(userInput) === secretNumber) {
+        alert('Win!');
+        break;
+    }
 }
