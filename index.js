11c2256aaf6748f1e0bc96f35d880abae2163033
Voronskoy <marskoy27@gmail.com>
2025-07-29 11:58:02 +0300

    function29-07

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
-// const start = 15;
-// const end = 20;
-// const secretNumber = 17;
-
-// for (let i = start; i < end; i++) {
-//     if (i === secretNumber) {
-//         continue;
-//         alert('after continue');
-//     }
-//     console.log(i);
-// }
-
-// debugger;
-// while (true) {
-//     const userInput = prompt('Enter number');
-    
-//     if (userInput === null) {
-//         alert('будь уважний!');
-//         continue;
-//     }
-
-//     if (Number(userInput) === secretNumber) {
-//         alert('Win!');
-//         break;
-//     }
-// }
- 
-const number1 = 2;
-const number2 = 7;
-const result = number1 * number2;
-
-while (true) {
-    const userInput = prompt(`скільки буде ${number1} * ${number2}?`);
-
-    if (userInput === null || userInput.trim() === '') {
-        alert('будь уважний!');
-        continue;
-    }
-
-    if (Number(userInput) === result) {
-        alert('Win! відповідь = ' + result);
-        break;
-    }
-
-    alert('Спробуй ще!');
+function convertKmInMl(km) {
+  return km * 1000;
 }
+const km = 5;
+// console.log(convertKmInMl(km));
+
+function isFreez(temp) {
+  return temp < 0;
+}
+// console.log(isFreez(0));
+
+function isEqual(a, b, c) {
+  return a === b && b === c;
+}
+// console.log(isEqual(3,2,2));
+
+function getDayNumber(dayNumber) {
+  // if (dayNumber === 6 || dayNumber === 7) {
+  //   return "weekend";
+  // } else {
+  //   return "workDay";
+  // }
+  return dayNumber === 6 || dayNumber === 7 ? "weekend" : "workDay";
+}
+// console.log(getDayNumber(7));
+
+function getTimeOfDay(hour) {
+  if (hour >= 6 && hour < 12) {
+    return "morning";
+  }
+  if (hour >= 12 && hour < 18) {
+    return "afternoon";
+  }
+  if (hour >= 18 && hour < 22) {
+    return "evening";
+  }
+  if ((hour >= 0 && hour < 6) || (hour >= 22 && hour < 24)) {
+    return "night";
+  }
+  return "wrong data";
+}
+// console.log(getTimeOfDay(2));
+
+function getGradeText(grade) {
+  if (grade >= 1 && grade < 4) {
+    return "bad";
+  }
+  if (grade >= 4 && grade < 7) {
+    return "normal";
+  }
+  if (grade >= 7 && grade < 10) {
+    return "good";
+  }
+  if (grade >= 10 && grade <= 12) {
+    return "amazing";
+  }
+  return "wrong number";
+}
+// console.log(getGradeText(15));
+
+function checkSpeedWiFi(speed, baseSpeed = 30) {
+  return speed >= baseSpeed;
+}
+console.log(checkSpeedWiFi(30, 40));
+console.log(checkSpeedWiFi(30));
+
