166f33601321d149fa878b239e31bd509a5966ce
Voronskoy <marskoy27@gmail.com>
2025-08-26 11:57:46 +0300

    arrayMethods

--- index.js
+++ index.js
@@ -1,45 +1,58 @@
-// Створюємо масив імен друзів
-const names = [];
-console.log(`Довжина масиву: ${names.length}`);
+// const games = ['Roblox', 'Minecraft'];
+// console.log(games);
+// games.push('GTA');
+// console.log(games);
+// const firstElem = games.shift();
+// games.push(firstElem);
+// console.log(games);
+// const newGame = 'Need For Speed'
+// if(games.includes(newGame) === false){
+//     games.unshift(newGame);
+// }
+// console.log(games);
+// const position = games.indexOf('GTA')
+// console.log(position);
+// games.splice(position+1, 0, 'GTA5', 'GTA6')
+// console.log(games);
 
-// Додаємо елементи в кінець масиву
-names.push('Anna', 'Olga');
-console.log('Після додавання в кінець:', names);
+// const cars = ["audi", "bmw"];
+// const userInput = prompt("Enter your Car:");
+// if (cars.includes(userInput)) {
+//   alert("вже є  масиві");
+// } else {
+//   cars.push(userInput);
+// }
+// console.log(cars);
 
-// Додаємо елементи на початок масиву
-names.unshift('Max', 'Fred');
-console.log('Після додавання на початок:', names);
+// const lastCar = cars.pop();
+// cars.unshift(lastCar);
+// console.log(cars);
 
-// Видаляємо елемент з кінця масиву
-const lastName = names.pop();
-console.log(`Видалений з кінця: ${lastName}`);
-console.log('Масив після видалення з кінця:', names);
+// const positionBmw = cars.indexOf("bmw");
+// cars.splice(positionBmw, 0, "Tesla");
+// console.log(cars);
 
-// Повертаємо видалений елемент на початок
-names.unshift(lastName);
-console.log('Після повернення на початок:', names);
+// const cleerAudi = cars.indexOf("audi");
+// cars.splice(cleerAudi, 1);
+// console.log(cars);
 
-// Видаляємо елемент з початку масиву
-const firstName = names.shift();
-console.log(`Видалений з початку: ${firstName}`);
-console.log('Масив після видалення з початку:', names);
-
-
-
-
-
-
-const fruits = [];
-
-console.log(fruits.length);
-
-fruits.push('яблуко', 'мандарин', 'кокос');
-fruits.unshift('манго', 'банан');
-
-const orange = fruits.pop();
-const banana = fruits.pop();
-
-const addFruit1 = fruits.unshift(orange);
-const addFruit2 = fruits.unshift(banana);
-
-console.log(fruits);
+const backpack = ["notebook", "pen", "bottle", "charger"];
+const posBottle = backpack.indexOf("bottle");
+backpack.splice(posBottle, 1);
+console.log(backpack);
+const posNotebook = backpack.indexOf("notebook");
+backpack.splice(posNotebook + 1, 0, "PowerBank");
+console.log(backpack);
+backpack.push("Pencel");
+console.log(backpack);
+if (backpack.includes("notebook")) {
+  backpack.unshift("tablet");
+} else {
+    backpack.push("tablet");
+}
+console.log(backpack);
+const posPen = backpack.indexOf('pen');
+const pen = backpack.splice(posPen, 1);
+console.log(pen[0]) ;
+backpack.unshift(pen[0]);
+console.log(backpack);
