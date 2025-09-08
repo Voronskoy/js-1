1b6e9d67b34586579e5fe3fafb814b2008b8a1b1
Voronskoy <marskoy27@gmail.com>
2025-09-08 17:49:55 +0300

    sort & reverse

--- index.js
+++ index.js
@@ -1,59 +1,21 @@
-const numbers = [2, 7, 10, 15, 25, 42];
-let count = 0;
-// numbers.forEach((number) => {
-//   if (number < 20) {
-//     count++
-//   };
-// });
-// console.log(count);
+//reverse, sort
 
-// numbers.forEach((number) => {
-//   if (number % 2 === 0) {
-//     count++;
-//   }
-// });
-// console.log(count);
+const numbers = [5, 172, 1, 10];
+const numbersSorted = numbers.toSorted((a, b) => a - b);
+const numbersSortedReverse = numbersSorted.toReversed();
 
-// numbers.forEach((number) => {
-//   if (number % 2 === 1) {
-//     console.log(number);
-//     ;
-//   }
-// });
-
-// numbers.forEach((number) => {
-//   if (number % 3 === 0) {
-//     console.log(number);
-//   }
-// });
-
-// let newNumbers = numbers.map((number) => number + 3);
-// console.log(newNumbers);
-
-// let newNumbers = numbers.map((number) => {
-//   if (number % 2 === 0) {
-//     return number / 2;
-//   }
-//   return number - 1;
-// });
-
-// const newNumbers = numbers.map((number) =>
-//   number % 2 === 0 ? number / 2 : number - 1
-// );
-
-// console.log(numbers);
-// console.log(newNumbers);
-
-const summa = numbers.reduce((accum, number) => accum + number, 0);
-console.log(summa);
-const average = summa / numbers.length;
-console.log(average);
-
-const numbersLessAverage = numbers.filter((number) => number < average);
-console.log(numbersLessAverage);
-
-
-const newNumbers = numbers.filter((number) => number % 8 === 0);
-console.log(newNumbers);
 console.log(numbers);
+console.log(numbersSorted);
+console.log(numbersSortedReverse);
 
+// метод sort ЗМІНЮЄ!!! поточний масив
+// console.log(numbers.sort());
+// console.log(numbers.sort((a, b) => a - b)); //0 1 -1
+// console.log(numbers);
+
+// console.log(numbers.sort((a, b) => b - a));
+// console.log(numbers);
+
+// метод reverse ЗМІНЮЄ!!! поточний масив
+// console.log(numbers.reverse());
+// console.log(numbers);
